85334550a01471d02a0c67f154b15ee1c99257f8
Rimolal <ila68801@gmail.com>
2026-02-28 13:17:30 +1000

    Lab1_1

--- Lab_1/Lab1_1.py
+++ Lab_1/Lab1_1.py
@@ -1,1 +1,11 @@
-print("qwertqqy")
+def unique(arr):
+    uni = set()
+    for i in range(0,4):
+        if arr[i] in uni:
+            return True
+        uni.add(arr[i])
+    return False
+
+
+arr = [1, 2, 3, 4]
+print(unique(arr))
